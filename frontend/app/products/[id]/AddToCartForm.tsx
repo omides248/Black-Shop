021e1e962f224f757ae8346456e01a4abb9c5549
omides248 <omidesmaeeli4@gmail.com>
2025-08-27 05:09:45 +0330

    Moved ui

--- frontend/app/products/[id]/AddToCartForm.tsx
+++
@@ -1,9 +0,0 @@
-import AddToCartButton from './AddToCartButton'; // We will create this next
-
-export default function AddToCartForm({ productId }: { productId: string }) {
-    return (
-        <div>
-            <AddToCartButton productId={productId} />
-        </div>
-    );
-}
--- not_moved_logic/api_gateway/delivery/graphql/graph/resolver.go
+++
@@ -1,7 +0,0 @@
-package graph
-
-import pbcatalog "black-shop/api/proto/catalog/v1"
-
-type Resolver struct {
-	CatalogClient pbcatalog.CatalogServiceClient
-}
